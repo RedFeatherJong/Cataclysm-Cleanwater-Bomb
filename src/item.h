0e11797ed81bd4e042c17bd18fed63eac73e2ce3
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2026-06-16 21:00:18 -0500

    Merge pull request #87666 from olivia-fl/count-by-charges-docs
    document that only liquid comestibles are counted by charges

--- src/item.h
+++ src/item.h
@@ -1111,7 +1111,7 @@ class item : public visitable
 
         /**
          * Return true if this item's type is counted by charges
-         * (true for stackable, ammo, or comestible)
+         * (true for stackable, ammo, or non-solid comestible)
          */
         bool count_by_charges() const;
 
